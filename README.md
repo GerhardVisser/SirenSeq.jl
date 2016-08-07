c76b40b8ebd9dfe671f76a9dfe39a47bccbb8056
Gerhard Visser <gerhard.visser@rocketmail.com>
2016-08-07 15:32:03 +1000

    added show methods for some Atoms

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SirenSeq
 
-A tool for composing and sequencing music using Julia.  The goal is to remove the need for GUI tools when composing, recording and sequencing music by doing it all using Julia scripts and the SirenSeq module.  At this time only MIDI file composition is implemented.  Later versions will work by using [csound](http://www.csounds.com/) for audio recording, playing and manipulation.
+A tool for composing and sequencing music using [Julia](http://julialang.org/).  The goal is to remove the need for GUI tools when composing, recording and sequencing music by doing it all using Julia scripts and the SirenSeq module.  At this time only MIDI file composition is implemented.  Later versions will work by using [csound](http://www.csounds.com/) for audio recording, playing and manipulation.
 
 - compatible only with Linux
 - the submodule *SirenSeq.Render* requires that [musescore](https://musescore.org/) is installed
--- src/display.jl
+++ src/display.jl
@@ -34,6 +34,110 @@ function Base.show(io::IO, x::Blank)
 	print(io,s)
 end
 
+function Base.show(io::IO, x::BankSel)
+	print(io,"Blank-Select:  ")
+	s = @sprintf "ch%d, " x.cha ; s = rpad(s,7)
+	print(io,s)	
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "bank = %d,  " x.bank
+	print(io,s)
+end
+
+function Base.show(io::IO, x::ProgSel)
+	print(io,"Program-Select:  ")
+	s = @sprintf "ch%d, " x.cha ; s = rpad(s,7)
+	print(io,s)	
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "prog = %d,  " x.prog
+	print(io,s)
+end
+
+function Base.show(io::IO, x::VolSet)
+	print(io,"Volume-Set:  ")
+	s = @sprintf "ch%d, " x.cha ; s = rpad(s,7)
+	print(io,s)	
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "vol = %.2f,  " x.vol
+	print(io,s)
+end
+
+function Base.show(io::IO, x::PitchWheel)
+	print(io,"Pitch-Wheel:  ")
+	s = @sprintf "ch%d, " x.cha ; s = rpad(s,7)
+	print(io,s)	
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "val = %.3f,  " (x.val-2^13)/(2^13)
+	print(io,s)
+end
+
+function Base.show(io::IO, x::ChanAfter)
+	print(io,"Channel-Aftertouch:  ")
+	s = @sprintf "ch%d, " x.cha ; s = rpad(s,7)
+	print(io,s)	
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "val = %.2f,  " x.val/127
+	print(io,s)
+end
+
+function Base.show(io::IO, x::Control7)
+	print(io,"Control-7bit:  ")
+	s = @sprintf "ch%d, " x.cha ; s = rpad(s,7)
+	print(io,s)	
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "num = %d,  " x.num
+	print(io,s)
+	s = @sprintf "val = %d,  " x.val
+	print(io,s)
+end
+
+function Base.show(io::IO, x::Control14)
+	print(io,"Control-14bit:  ")
+	s = @sprintf "ch%d, " x.cha ; s = rpad(s,7)
+	print(io,s)	
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "num = %d,  " x.num
+	print(io,s)
+	s = @sprintf "val = %d,  " x.val
+	print(io,s)
+end
+
+function Base.show(io::IO, x::Marker)
+	print(io,"Marker:  ")
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "name = %s,  " x.name
+	print(io,s)
+end
+
+function Base.show(io::IO, x::TimeSignature)
+	print(io,"Time-Signature:  ")
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "num = %d,  " x.num
+	print(io,s)
+	s = @sprintf "den = %d,  " x.den
+	print(io,s)
+	s = @sprintf "clocks = %d,  " x.clocks
+	print(io,s)
+	s = @sprintf "sub = %d,  " x.sub
+	print(io,s)
+end
+
+function Base.show(io::IO, x::Tempo)
+	print(io,"Tempo-Set:  ")
+	s = @sprintf "ofs = %d + %s, " div(x.ofs,1//1) "$(mod(x.ofs,1//1))" ; s = rpad(s,18)
+	print(io,s)
+	s = @sprintf "bpm = %d,  " x.bpm
+	print(io,s)
+end
+
 
 
 
